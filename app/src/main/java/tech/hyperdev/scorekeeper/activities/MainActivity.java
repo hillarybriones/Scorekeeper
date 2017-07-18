b2d93240b4646cbfef48e484a557420c3e87cd76
hillarybriones <hillarysabanatebriones@gmail.com>
2017-07-18 18:18:08 +0800

    (3) Modify team names of each score fragment.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -5,22 +5,28 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
     private Fragment fragmentA,fragmentB;
+    //private ScoreFragment objA;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmentA=new ScoreFragment();
-        fragmentB=new ScoreFragment();
+        fragmentA=ScoreFragment.newInstance("Team 1");
+        fragmentB=ScoreFragment.newInstance("Team 2");
+
+
 
         FragmentManager fragmentManager=getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.fragment1,fragmentA)
                                           .replace(R.id.fragment2,fragmentB)
                 .commit();
+
     }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -12,7 +13,8 @@ import tech.hyperdev.scorekeeper.R;
  * A simple {@link Fragment} subclass.
  */
 public class ScoreFragment extends Fragment {
-
+private TextView textView;
+    private String name;
     public ScoreFragment() {
         // Required empty public constructor
     }
@@ -21,8 +23,25 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
-    }
+       View v= inflater.inflate(R.layout.fragment_score, container, false);
+        textView=(TextView)v.findViewById(R.id.tvTeamName);
+        readBundle(getArguments());
+        textView.setText(name);
+        return v;
 
+    }
+public static  ScoreFragment newInstance(String name ){
+    Bundle bundle =new Bundle();
+    bundle.putString("name",name);
+    ScoreFragment fragment=new ScoreFragment();
+    fragment.setArguments(bundle);
+    return fragment;
+}
+    private void readBundle(Bundle bundle) {
+        if (bundle != null) {
+            name = bundle.getString("name");
+
+        }
+    }
 
 }
